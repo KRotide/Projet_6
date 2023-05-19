b67bda67a2b5f632d95b8752e683a05b6ba03ffb
KRotide <Cheepster38@hotmail.fr>
2023-05-19 18:33:50 +0200

    Création des filtres

--- gallery.js
+++ gallery.js
@@ -1,3 +1,50 @@
+// Création d'un Set pour stocker les catégories uniques
+const categoriesSet = new Set();
+
+async function categories() {
+    try {
+        // Appel de l'API pour récupérer les catégories
+        const response = await fetch("http://localhost:5678/api/categories");
+        const categoriesData = await response.json();
+
+        // Ajout manuel d'une nouvelle catégorie
+        const nouvelleCategorie = { id: "4", name: "Tous" };
+        categoriesSet.add(nouvelleCategorie);
+
+        // Ajout des catégories de l'API dans le Set
+        for (const categorie of categoriesData) {
+            categoriesSet.add(categorie);
+        }
+
+        genererCategories(categoriesSet);
+
+        return categoriesSet;
+    }catch(err){
+        throw new Error("Something went wrong");
+    }
+};
+
+function genererCategories(categories){
+    // Récupération de l'élément du DOM qui accueillera les catégories
+    const divFiltres = document.querySelector(".filters");
+
+    for (const categorie of categories) {
+        // Création d’une balise dédiée à une catégorie
+        const categorieElement = document.createElement("ul");
+        categorieElement.dataset.id = categorie.id;
+
+        // Création de la balise
+        const nomElement = document.createElement("li");
+        nomElement.innerHTML = categorie.name;
+
+        // On rattache les balises à leur parent
+        divFiltres.appendChild(categorieElement);
+        categorieElement.appendChild(nomElement);
+    }
+};
+
+categories();
+
 async function works() {
     try {
         // Appel de l'API pour récupérer les travaux
@@ -27,13 +74,13 @@ function genererProjets(projets){
         const imageElement = document.createElement("img");
         imageElement.src = projet.imageUrl;
 
-        const nomElement = document.createElement("figcaption");
-        nomElement.innerHTML = projet.title;
+        const titleElement = document.createElement("figcaption");
+        titleElement.innerHTML = projet.title;
 
         // On rattache les balises à leur parent
         divGalerie.appendChild(projetElement);
         projetElement.appendChild(imageElement);
-        projetElement.appendChild(nomElement);
+        projetElement.appendChild(titleElement);
     }
 };
 
